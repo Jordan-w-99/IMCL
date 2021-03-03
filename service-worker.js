37f17a05a327d0fccb8fcf7fd761508b182fa45f
Jordan <jordanw99@hotmail.co.uk>
2021-03-03 01:41:27 +0000

    loading fix?

--- service-worker.js
+++ service-worker.js
@@ -2,5 +2,5 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
-    new workbox.strategies.CacheFirst() // Maybe NetworkFirst
+    new workbox.strategies.NetworkFirst() // Maybe NetworkFirst
 );
